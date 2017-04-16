9f5dc039e2636f2c20abd9b6ef230bc0ac0dd5f8
Morgan Leborgne <leborgne@zib.de>
2017-04-16 16:15:35 +0200

    rework button.h.

--- Button.h
+++ Button.h
@@ -1,61 +1,25 @@
-/* 
-* button.h
-*
-* Created: 04.05.2015 21:38:42
-* Author: Yogi
-*/
-
-#ifndef __BUTTON_H__
-#define __BUTTON_H__
+#pragma once
 
 #include <Arduino.h>
 
-#define debounceDelay 50
-
-typedef void (*ActionCb)(void);
-
 class Button {
 public:
-    Button(byte p, ActionCb a)
-        : debounce(0)
-        , state(1)
-        , lastState(1)
+    Button(char p)
+        : state(0)
         , pin(p)
-        , action(a)
     {
         pinMode(pin, INPUT_PULLUP);
     }
 
-    void check()
+    bool wasPressed()
     {
-        byte b = digitalRead(pin);
-        long t = millis();
-
-        if (b != lastState) {
-            debounce = t;
-        }
-
-        if ((t - debounce) > debounceDelay) {
-            if (b != state) {
-                state = b;
-
-                if (!state) {
-                    (*action)();
-                }
-            }
-        }
-
-        lastState = b;
+        state = (state << 1) | !digitalRead(pin) | 0xe000;
+        return (state == 0xf000);
     }
 
 private:
-    long debounce;
-    byte state : 1;
-    byte lastState : 1;
-    byte pin;
-    ActionCb action;
+    unsigned short state;
+    char pin;
     Button(const Button& c);
     Button& operator=(const Button& c);
-}; //button
-
-#endif //__BUTTON_H__
+};
